38238f68a3265f35d08642e5eb9e7ce62d401313
ciao999 <claireotte8@gmail.com>
2025-05-14 22:14:11 +0200

    Hausaufgabe

--- exercise5.py
+++ exercise5.py
@@ -1,23 +1,16 @@
 import matplotlib.pyplot as plt
 
-#Parameter
+#formel für geometrische Reihe ar^n
 a = 1
 r = 0.5
 n = 10
 
-#leere Liste
 s_n = []
 
-#Summe Schritt für Schritt berechnen 
-summe = 0 
+summe = 0
 for k in range(n):
-    summe += a * r**k
+    summe += a*r**k
     s_n.append(summe)
     
-#Plot der Werte
-plt.plot(s_n, marker="o")
-plt.title("Geometrische Reihe a=1, r=0.5")
-plt.xlabel("Anzahl Terme n")
-plt.ylabel("Summe s_n")
-plt.grid(True)
-
+plt.plot(s_n)
+    
